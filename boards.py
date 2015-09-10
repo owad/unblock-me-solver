1731479fea1f0e76b41443faa65824b966446c37
Łukasz Lechowicz <llechowicz@gmail.com>
2015-09-10 19:06:49 +0100

    Add example boards.

---
+++ boards.py
@@ -0,0 +1,62 @@
+BOARD1 = """
+* a a a * *
+* * * b b c
+s s * e d c
+* g * e d c
+* g * e * *
+* * f f f *
+"""
+
+
+TEST_BOARD0 = """
+* * * * * *
+* * * * * *
+z z * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+"""
+
+TEST_BOARD1 = """
+* * * * * *
+* * * * * *
+z z * a * *
+* * * a * *
+* * * * * *
+* * * * * *
+"""
+
+TEST_BOARD2 = """
+* * * * * *
+* * a a a *
+z z * b * *
+* * * b * *
+* * * * * *
+* * * * * *
+"""
+
+TEST_BOARD3 = """
+* * * * * *
+* * a a a *
+z z * b * *
+* * * b * *
+* * * c c *
+* * * * * *
+"""
+
+TEST_BOARD4 = """
+* * * * * *
+* * a a a *
+z z * b * *
+* * * b * *
+* * e c c d
+* * e * * d
+"""
+
+TEST_BOARDS = [
+    TEST_BOARD0,
+    TEST_BOARD1,
+    TEST_BOARD2,
+    TEST_BOARD3,
+    TEST_BOARD4,
+]
